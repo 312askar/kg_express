5eff2348d24ae36e03eaaa0f3cd576bb638e02df
312askar <askar.shekenov@yandex.ru>
2022-04-28 22:11:58 +0600

    add register and login for user

--- backend/apps/product/views.py
+++ backend/apps/product/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 import json
 # Create your views here.
 from .models import SubCategory
@@ -8,5 +9,9 @@ from .models import SubCategory
 def get_subcategories(request):
     id = request.GET.get('id', '')
     result = list(SubCategory.objects.filter(
-    category_id=int(id)).values('id', 'name'))
+        category_id=int(id)).values('id', 'name'))
     return HttpResponse(json.dumps(result), content_type="application/json")
+
+
+class IndexPage(TemplateView):
+    template_name = "index.html"
